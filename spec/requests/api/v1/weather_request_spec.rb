1cb74459515397e30b2eaff2f94a11492b1bc172
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-06-01 16:38:47 -0600

    Creates request spec for forecast api endpoint

---
+++ spec/requests/api/v1/weather_request_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe 'Weather API' do
+  it "gets the weather based on a city and state" do
+    city = create(:city, name: "Denver", state: "CO")
+
+    get "/api/v1/forecast?location=denver,co"
+
+    expect(response).to be_successful
+
+    weather = JSON.parse(response)
+    expect(weather[:data]).to be_truthy
+  end
+end
