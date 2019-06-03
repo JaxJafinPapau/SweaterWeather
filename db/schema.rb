5f013d9301c20c666cab9a72234ea2415a33150d
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-06-03 15:18:49 -0600

    runs pending migration and alters test to pass city object instead of location hash

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_03_195524) do
+ActiveRecord::Schema.define(version: 2019_06_03_205301) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_06_03_195524) do
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
+    t.string "strf_description"
   end
 
 end
