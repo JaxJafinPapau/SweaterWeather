ff0b4728ed3eb0e951291e5cfd7aa19217c0833b
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-06-02 18:04:56 -0600

    Updates weather facade to pass the json from a CurrentWeather instance method, not the object itself. Reorganizes method names to make more sense.

--- app/facades/weather_facade.rb
+++ app/facades/weather_facade.rb
@@ -1,19 +1,21 @@
 class WeatherFacade
+  attr_reader :id
   def initialize(params)
     @params = params
+    @id = 0
   end
 
   def current_weather
-    CurrentWeather.new(weather)
+    CurrentWeather.new(dark_sky.weather, google.user_formatted_location)
   end
 
   private
 
-    def location
+    def google
       @_location ||= GoogleGeocodingService.new(@params[:location])
     end
 
-    def weather
-      @_weather ||= DarkSkyService.new(location)
+    def dark_sky
+      @_weather ||= DarkSkyService.new(google.lat_long_location)
     end
 end
