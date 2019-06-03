9b9e88abfe5ff0d1bcf8ec059980799a7cc9d4f3
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-06-02 19:18:17 -0600

    adds hourlies and details attributes to serializer

--- app/serializers/weather_serializer.rb
+++ app/serializers/weather_serializer.rb
@@ -1,4 +1,7 @@
 class WeatherSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :current_weather
+  attributes :id,
+             :current_weather,
+             :details,
+             :hourly_forecast
 end
