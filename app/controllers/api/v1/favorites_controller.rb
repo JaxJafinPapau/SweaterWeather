79da24dfae58bddbfb531c7c2e0a0b2842381c7a
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-06-04 14:31:31 -0600

    Makes strong params in controller and implements auth so only a current user can create a favorite

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,5 +1,16 @@
 class Api::V1::FavoritesController < ApplicationController
   def create
-    facade = FavoritesFacade.new(favorite_params)
+    if current_user
+      facade = FavoritesFacade.new(favorite_params)
+      render status: 200, json: FavoritesSerializer.new(facade)
+    else
+      render status: 404
+    end
   end
+
+  private
+
+    def favorite_params
+      params.permit(:location, :api_key, :action)
+    end
 end
