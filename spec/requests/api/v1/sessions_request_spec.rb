393197a15c5d6efb45a9798ad3f6dc7ca84e6307
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-06-03 22:47:25 -0600

    updates spec

--- spec/requests/api/v1/sessions_request_spec.rb
+++ spec/requests/api/v1/sessions_request_spec.rb
@@ -6,8 +6,8 @@ describe 'Weather API' do
                       "email": "whatever@example.com",
                    "password": "password"
     }.to_json
-    
-    create(:user, email: test_user_data['email'], password: test_user_data['password'], api_key: '12345')
+
+    create(:user, email: "whatever@example.com", password: "password", api_key: '12345')
 
     headers = { "CONTENT_TYPE" => "application/json" }
 
@@ -16,6 +16,6 @@ describe 'Weather API' do
     expect(response).to be_successful
     expect(response.status).to be(200)
     raw_key = JSON.parse(response.body)
-    expect(raw_key['data']['attributes']['api_key']).to be('12345')
+    expect(raw_key['data']['attributes']['api_key']).to eq('12345')
   end
 end
