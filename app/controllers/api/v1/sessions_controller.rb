ba7fa85e6b64d8531fed2c7b5320f3e90879407a
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-06-03 22:46:47 -0600

    adds route and sessions controller

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::SessionsController < ApplicationController
+  def create
+    facade = SessionsFacade.new(user_params)
+    if facade.user
+      session[:user_id] = facade.user.id
+      render status: 200, json: SessionsSerializer.new(facade)
+    else
+      render status: 404
+    end
+  end
+
+  private
+
+    def user_params
+      params.permit(:email, :password)
+    end
+end
