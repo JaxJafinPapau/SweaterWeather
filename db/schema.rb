c21e7e153b30512d4562ec6c2ff375afb09d141b
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-06-03 21:40:51 -0600

    Creates users table, updates validations and spec.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_03_205301) do
+ActiveRecord::Schema.define(version: 2019_06_04_015151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 2019_06_03_205301) do
     t.float "latitude"
     t.float "longitude"
     t.string "strf_description"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.string "api_key"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
